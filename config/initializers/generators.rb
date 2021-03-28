7479dd3cb2777aa69c9b48f6a1ff8ad0e9154cba
motchang <motchang@gmail.com>
2021-03-28 15:56:20 +0900

    Use slim

---
+++ config/initializers/generators.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+Rails.application.config.generators do |gen|
+  gen.template_engine = :slim
+  gen.jbuilder false
+  gen.assets false
+  gen.helper false
+
+  # Don't generate system test files.
+  gen.system_tests = false
+  gen.test_framework :rspec,
+                     javascripts:   false,
+                     stylesheets:   false,
+                     helper:        false,
+                     fixtures:      true,
+                     view_specs:    false,
+                     helper_specs:  true,
+                     routing_specs: false
+  gen.factory_bot dir: 'spec/factories'
+end
